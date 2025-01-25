6556f2e4630ffe873257378f3175b4a45a55b15f
octaviusthe3rd <a.chagaresa@alustudent.com>
2025-01-26 00:36:31 +0200

    task#6

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage or DBStorage"""
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 storage_type = os.getenv('HBNB_TYPE_STORAGE')
+print(f"HBNB_TYPE_STORAGE: {storage_type}")  # Debugging line to print the storage type
 
 if storage_type == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
+    print("DBStorage initialized")
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
+    print("FileStorage initialized")
 
 storage.reload()
+print("Storage reloaded")
