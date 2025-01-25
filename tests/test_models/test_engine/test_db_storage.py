1e1735ec35ac7e211d8e770ed9bf10b55c0ab6e9
octaviusthe3rd <a.chagaresa@alustudent.com>
2025-01-25 18:07:32 +0200

    task#1

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -3,10 +3,11 @@ import os
 import unittest
 from models.state import State
 from models.engine.db_storage import DBStorage
-
+from sqlalchemy.orm import sessionmaker
 
 storage = DBStorage()
-
+Session = sessionmaker(bind=storage._DBStorage__engine)
+session = Session()
 
 @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db', 'DB Storage test')
 class TestDBStorage(unittest.TestCase):
@@ -15,56 +16,32 @@ class TestDBStorage(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         """Setting up the test environment"""
-        storage.reload()
-        if not isinstance(storage, DBStorage):
-            raise unittest.SkipTest("Not using DBStorage")
+        cls.session = session
 
-    def setUp(self):
-        if isinstance(storage, DBStorage):
-            self.session = storage._DBStorage__session
-            self.session.begin()
-
-    def tearDown(self):
-        """Rolls back a transaction"""
-        if hasattr(self, 'session'):
-            self.session.rollback()
+    @classmethod
+    def tearDownClass(cls):
+        """Tearing down the test environment"""
+        cls.session.close()
 
     def test_add_and_commit(self):
         """Testing adding and committing objects to Database"""
         initial_count = self.session.query(State).count()
-        new_state = State(name="Texas")
-        self.session.add(new_state)
-        self.session.commit()
-        self.assertEqual(self.session.query(State).count(), initial_count + 1)
-        self.session.delete(new_state)
-        self.session.commit()
-
-    def test_delete_state(self):
-        """Testing deleting a State from the Database"""
-        initial_count = self.session.query(State).count()
-
         new_state = State(name="California")
         self.session.add(new_state)
         self.session.commit()
+        new_count = self.session.query(State).count()
+        self.assertEqual(initial_count + 1, new_count)
 
-        self.assertEqual(self.session.query(State).count(), initial_count + 1)
-
-        # Deletes then verifies removal
-        self.session.delete(new_state)
-        self.session.commit()
-        self.assertEqual(self.session.query(State).count(), initial_count)
-
-    def test_get_state(self):
-        """Testing retrieving a State from the Database"""
-        new_state = State(name="Nevada")
+    def test_delete_state(self):
+        """Testing deleting objects from Database"""
+        new_state = State(name="California")
         self.session.add(new_state)
         self.session.commit()
-
-        # Retrieves and verifies retrieval
-        retrieved_state = self.session.query(State).get(new_state.id)
-        self.assertEqual(retrieved_state.id, new_state.id)
-        self.assertEqual(retrieved_state.name, "Nevada")
-
-        # Cleanup
+        initial_count = self.session.query(State).count()
         self.session.delete(new_state)
         self.session.commit()
+        new_count = self.session.query(State).count()
+        self.assertEqual(initial_count - 1, new_count)
+
+if __name__ == "__main__":
+    unittest.main()
