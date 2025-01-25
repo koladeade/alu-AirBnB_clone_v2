d2acac39fade3cb550cd90359ca8beae74c04065
octaviusthe3rd <a.chagaresa@alustudent.com>
2025-01-25 23:15:25 +0200

    reviews

--- models/place.py
+++ models/place.py
@@ -1,18 +1,33 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.review import Review
+import models
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
+import os
 
 
 class Place(BaseModel):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    __tablename__ = 'places'
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    reviews = relationship('Review', backref='place', cascade='all, delete-orphan')
     amenity_ids = []
+    
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        @property
+        def reviews(self):
+            """ Getter attribute that returns the list of Review instances """
+            all_reviews = list(models.storage.all(Review).values())
+            review_list = [review for review in all_reviews if review.place_id == self.id]
+            return review_list
