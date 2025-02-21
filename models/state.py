6f95b1d73d40e0e10df70665ac0037812a3d5a4e
kolawizzy <mac@KLD.local>
2025-02-22 00:24:48 +0200

    correction

--- models/state.py
+++ models/state.py
@@ -1,26 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-import os
-from models import storage
-from models.city import City
+from models.base_model import BaseModel
 
-class State(BaseModel, Base):
+
+class State(BaseModel):
     """ State class """
-    __tablename__ = 'states'
-    
-    name = Column(String(128), nullable=False)
-    
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', backref='state', cascade='all, delete-orphan')
-    else:
-        @property
-        def cities(self):
-            """Getter method to retrieve list of cities related to this state"""
-            city_list = []
-            for city in storage.all(City).values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+    name = ""
